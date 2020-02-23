5be6a59d3308acbe0e8269d1a72337c2d519ae46
Oleg Yunin <oayunin@gmail.com>
2020-02-24 00:44:45 +0300

    minimal viable product (with Bearer auth!)

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from aiohttp import web
 import os
 
+import pdf2image
+from aiohttp import web
 
-async def hello(request):
-    return web.Response(text="Hello, world")
+import decode_code128_from_pdf
 
 
-app = web.Application()
-app.add_routes([web.get('/', hello)])
+def check_auth(request: web.Request):
+    auth_value = request.headers.get('Authorization')
+    correct_auth_value = 'Bearer %s' % os.environ['SECRET_TOKEN']
+    if not auth_value == correct_auth_value:
+        raise web.HTTPForbidden()
+
+
+async def hello(request: web.Request):
+    check_auth(request)
+
+    res = await request.post()
+    bytes_data = res['data'].file.read()
+    code_data_list = decode_code128_from_pdf.extract_code128(input=bytes_data, extractor=pdf2image.convert_from_bytes)
+    return web.json_response(code_data_list)
+
+
+app = web.Application(client_max_size=1024*1024*32)
+app.add_routes([web.post('/', hello)])
+
 
 if __name__ == '__main__':
     web.run_app(app, port=int(os.getenv('PORT', '8080')))
--- decode_code128_from_pdf.py
+++ decode_code128_from_pdf.py
@@ -14,12 +14,10 @@ def get_parser():
     return parser
 
 
-def extract_from_path(pdf_path):
+def extract_code128(input, extractor=pdf2image.convert_from_path):
     result = []
-    for page_num, page in enumerate(pdf2image.convert_from_path(pdf_path), 1):
+    for page_num, page in enumerate(extractor(input), 1):
         img = np.asarray(page)
-        if page_num == 5:
-            x = ''
         for barcode_ind, decoded_object in enumerate(decode(img, symbols=[ZBarSymbol.CODE128])):
             result.append({'page': page_num, 'barcode_ind': barcode_ind, 'data': decoded_object.data.decode()})
 
@@ -27,7 +25,7 @@ def extract_from_path(pdf_path):
 
 
 def main(pdf_path):
-    for code_data in extract_from_path(pdf_path):
+    for code_data in extract_code128(pdf_path):
         print('Page: #{page}, data: {data}'.format(**code_data))
 
 
