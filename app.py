0f923bfb691cd632d902af1bf91b6a680026fe89
Oleg Yunin <oayunin@gmail.com>
2020-02-24 00:57:34 +0300

    POST /decode endpoint now provides decode service

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def check_auth(request: web.Request):
         raise web.HTTPForbidden()
 
 
-async def hello(request: web.Request):
+async def decode_handler(request: web.Request):
     check_auth(request)
 
     res = await request.post()
@@ -22,13 +22,14 @@ async def hello(request: web.Request):
     return web.json_response(code_data_list)
 
 
+
 async def ping_handler(request: web.Request):
     return web.Response(text='pong\n')
 
 
 app = web.Application(client_max_size=1024*1024*32)
-app.add_routes([web.post('/', hello)])
 app.add_routes([web.get('/ping', ping_handler)])
+app.add_routes([web.post('/decode', decode_handler)])
 
 
 if __name__ == '__main__':
